36ca2a53c199f7e736c1a8a0785a6314e466dac9
mohGhazala96 <moh.abou.ghazala@gmail.com>
2020-04-24 15:08:20 +0200

    added buttons

---
+++ Set/Set/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Mohamed Aboughazala on 4/24/20.
+//  Copyright © 2020 Mohamed Aboughazala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var toBeDealtCards: [UIButton]!
+    @IBOutlet var onScreenCards: [UIButton]!
+    @IBAction func touchCard(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
