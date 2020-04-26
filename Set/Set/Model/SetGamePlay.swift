808a1e694cef77dcf849d6adecdcd8d6ebef8a79
mohGhazala96 <moh.abou.ghazala@gmail.com>
2020-04-26 18:07:02 +0200

    refactored evaluation function

--- Set/Set/Model/SetGamePlay.swift
+++ Set/Set/Model/SetGamePlay.swift
@@ -35,35 +35,23 @@ class SetGamePlay {
         return score
     }
     
-    func evaluateShapes(_ v1:  Card.shapes, _ v2: Card.shapes, _ v3: Card.shapes) -> Bool {
-        return (v1==v2 && v1==v3) || ((v1 != v2) && (v1 != v3) && (v2 != v3))
-    }
-    
-    func evaluateShades(_ v1:  Card.shading, _ v2: Card.shading, _ v3: Card.shading) -> Bool {
-        return (v1==v2 && v1==v3) || ((v1 != v2) && (v1 != v3) && (v2 != v3))
-    }
-    
-    func evaluateColor(_ v1:  Card.colors, _ v2: Card.colors, _ v3: Card.colors) -> Bool {
-        return (v1==v2 && v1==v3) || ((v1 != v2) && (v1 != v3) && (v2 != v3))
-    }
-    
-    func evaluateNumberOfSymbols(_ v1:  Int, _ v2: Int, _ v3: Int) -> Bool {
+    func evaluateFeatures(_ v1:  Int, _ v2: Int, _ v3: Int) -> Bool {
         return (v1==v2 && v1==v3) || ((v1 != v2) && (v1 != v3) && (v2 != v3))
     }
     
     func doSelectedCardsMatch() -> Bool {
         
         // all three dirrenet/same shapes
-        let evalShapes = evaluateShapes(selectedCards[0].shape, selectedCards[1].shape,selectedCards[2].shape)
+        let evalShapes = evaluateFeatures(selectedCards[0].shape.hashValue, selectedCards[1].shape.hashValue,selectedCards[2].shape.hashValue)
 
         // all three dirrenet/same shade
-        let evalShades = evaluateShades(selectedCards[0].shade, selectedCards[1].shade,selectedCards[2].shade)
+        let evalShades = evaluateFeatures(selectedCards[0].shade.hashValue, selectedCards[1].shade.hashValue,selectedCards[2].shade.hashValue)
         
         // all three dirrenet/same color
-        let evalColor = evaluateColor(selectedCards[0].color, selectedCards[1].color,selectedCards[2].color)
+        let evalColor = evaluateFeatures(selectedCards[0].color.hashValue, selectedCards[1].color.hashValue,selectedCards[2].color.hashValue)
 
         // all three dirrenet/same number
-        let evalNumber = evaluateNumberOfSymbols(selectedCards[0].numberOfSymbols, selectedCards[1].numberOfSymbols,selectedCards[2].numberOfSymbols)
+        let evalNumber = evaluateFeatures(selectedCards[0].numberOfSymbols.hashValue, selectedCards[1].numberOfSymbols.hashValue,selectedCards[2].numberOfSymbols.hashValue)
 
         return evalShapes && evalShades && evalColor && evalNumber
     }
